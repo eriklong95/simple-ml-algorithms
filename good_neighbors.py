4344f03b1cc9d4f60028dc2bffbdc9dc8112a1e0
Erik Lange <erik.lange@fasttvnet.dk>
2021-07-16 08:47:24 +0200

    several algorithms for regression added

--- good_neighbors.py
+++ good_neighbors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -47,8 +48,8 @@ def exp_weighted_nearest_neighbors(training_set, x):
 
 def exp_sqr_weighted_nearest_neighbors(training_set, x):
     ''' Predict response for given predictor x by calculating a weighted sum
-            of the responses in the training set.
-        '''
+        of the responses in the training set.
+    '''
 
     a = np.exp(- np.abs(x - pd.to_numeric(training_set.loc[:, 'x'])) ** 2)
     numerator = np.dot(a, pd.to_numeric(training_set.loc[:, 'y']))
@@ -57,8 +58,17 @@ def exp_sqr_weighted_nearest_neighbors(training_set, x):
 
 
 def ols(training_set, x):
-    pass
+    n = len(training_set)
+    xs = np.array(training_set.loc[:, 'x'])
+    ys = np.array(training_set.loc[:, 'y'])
 
+    matrix = np.empty(shape=(n, 2))
+    matrix[:, 0] = [1 for _ in range(n)]
+    matrix[:, 1] = xs
+
+    hat_beta = inv(matrix.T.dot(matrix)).dot(matrix.T).dot(ys)
+
+    return hat_beta.T.dot(np.array([1, x]))
 
 if __name__ == '__main__':
 
@@ -75,6 +85,8 @@ if __name__ == '__main__':
     training_set.loc[:, 'y'] = y_obs
     print(training_set)
 
+    print(ols(training_set, 10))
+
     # Plot data set and predictions.
     fig, ax = plt.subplots()
 
@@ -83,11 +95,12 @@ if __name__ == '__main__':
 
     # Predictions
     x_values = np.linspace(0, N, 100)
-    ax.scatter(x_values, [nearest_neighbors(training_set, x) for x in x_values], color='green', s=1)
-    ax.scatter(x_values, [weighted_nearest_neighbors(training_set, x) for x in x_values], color='red', s=1)
-    ax.scatter(x_values, [sqr_weighted_nearest_neighbors(training_set, x) for x in x_values], color='purple', s=1)
+    # ax.scatter(x_values, [nearest_neighbors(training_set, x) for x in x_values], color='green', s=1)
+    # ax.scatter(x_values, [weighted_nearest_neighbors(training_set, x) for x in x_values], color='red', s=1)
+    # ax.scatter(x_values, [sqr_weighted_nearest_neighbors(training_set, x) for x in x_values], color='purple', s=1)
     ax.scatter(x_values, [exp_weighted_nearest_neighbors(training_set, x) for x in x_values], color='yellow', s=1)
     ax.scatter(x_values, [exp_sqr_weighted_nearest_neighbors(training_set, x) for x in x_values], color='pink', s=1)
+    ax.plot(x_values, [ols(training_set, x) for x in x_values], color='black')
     plt.show()
 
 
