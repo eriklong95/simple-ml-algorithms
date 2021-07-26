a2802ad938e0c45e2a7a390def046c043d7ca7f4
Erik Lange <erik.lange@fasttvnet.dk>
2021-07-26 09:28:42 +0200

    demonstration.py added

---
+++ demonstration.py
@@ -0,0 +1,52 @@
+import numpy as np
+from numpy.linalg import inv
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+from good_neighbors import predict
+
+# Simulate data set
+slope = 1.5
+intercept = 1
+N = 25
+noise = 1
+
+data = pd.DataFrame(index=range(N), columns=['x', 'y'])
+
+rv = stats.norm(0, noise)
+x_obs = np.array(range(N)) + np.array(stats.norm.rvs(size=N))
+y_obs = intercept + slope * np.array(range(N)) + np.array(stats.norm.rvs(size=N))
+data.loc[:, 'x'] = x_obs
+data.loc[:, 'y'] = y_obs
+print("The data set:")
+print(data)
+
+# Extract training set - randomly select 75 % of the observations to train on
+training_set = data.sample(frac=0.75)
+test_set = data.drop(index=training_set.index)  # the rest of the data set becomes the test set
+print("The training set:")
+print(training_set)
+print("The test set")
+print(test_set)
+
+# Plot
+X_train = training_set.loc[:, 'x']
+y_train = training_set.loc[:, 'y']
+X_test = test_set.loc[:, 'x']
+y_test = test_set.loc[:, 'y']
+
+fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2, constrained_layout=True)
+
+methods = ["nearest_neighbor", "weighted_nearest_neighbors", "sqr_weighted_nearest_neighbors", "exp_weighted_nearest_neighbors"]
+axs = [ax1, ax2, ax3, ax4]
+for i in range(4):
+    axs[i].set_title(methods[i])
+    axs[i].scatter(X_train, y_train, s=5, color='blue')
+    axs[i].scatter(X_test, y_test, color='red')
+    axs[i].scatter(X_test, [predict(training_set, x, method=methods[i]) for x in X_test], color='green')
+    # TODO: legends
+
+x_values = np.linspace(0, N, 1000)
+for ax in axs:
+    ax.plot(x_values, [predict(training_set, x, method="ols") for x in x_values], color='black')
+plt.show()
--- good_neighbors.py
+++ good_neighbors.py
@@ -5,7 +5,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 
-def nearest_neighbors(training_set, x):  # training_set must be pd.DataFrame with columns 'x' and 'y'.
+def nearest_neighbor(training_set, x):  # training_set must be pd.DataFrame with columns 'x' and 'y'.
     ''' Predict response for given predictor x by finding data point in training set
         with predictor nearest x.
     '''
@@ -46,17 +46,6 @@ def exp_weighted_nearest_neighbors(training_set, x):
     return numerator / denominator
 
 
-def exp_sqr_weighted_nearest_neighbors(training_set, x):
-    ''' Predict response for given predictor x by calculating a weighted sum
-        of the responses in the training set.
-    '''
-
-    a = np.exp(- np.abs(x - pd.to_numeric(training_set.loc[:, 'x'])) ** 2)
-    numerator = np.dot(a, pd.to_numeric(training_set.loc[:, 'y']))
-    denominator = a.sum()
-    return numerator / denominator
-
-
 def ols(training_set, x):
     n = len(training_set)
     xs = np.array(training_set.loc[:, 'x'])
@@ -70,37 +59,18 @@ def ols(training_set, x):
 
     return hat_beta.T.dot(np.array([1, x]))
 
-if __name__ == '__main__':
 
-    # Simulate a training set.
-    slope = 1.5
-    intercept = 1
-    N = 25
-    training_set = pd.DataFrame(index=range(N), columns=['x', 'y'])
+def predict(training_set, x, method="nearest_neighbor"):
+    if method == "nearest_neighbor":
+        return nearest_neighbor(training_set, x)
+    elif method == "weighted_nearest_neighbors":
+        return weighted_nearest_neighbors(training_set, x)
+    elif method == "sqr_weighted_nearest_neighbors":
+        return sqr_weighted_nearest_neighbors(training_set, x)
+    elif method == "exp_weighted_nearest_neighbors":
+        return exp_weighted_nearest_neighbors(training_set, x)
+    else:
+        return ols(training_set, x)
 
-    rv = stats.norm(0, 10)
-    x_obs = np.array(range(N)) + np.array(stats.norm.rvs(size=N))
-    y_obs = intercept + slope * np.array(range(N)) + np.array(stats.norm.rvs(size=N))
-    training_set.loc[:, 'x'] = x_obs
-    training_set.loc[:, 'y'] = y_obs
-    print(training_set)
-
-    print(ols(training_set, 10))
-
-    # Plot data set and predictions.
-    fig, ax = plt.subplots()
-
-    # Training set
-    ax.scatter(x_obs, y_obs)
-
-    # Predictions
-    x_values = np.linspace(0, N, 100)
-    # ax.scatter(x_values, [nearest_neighbors(training_set, x) for x in x_values], color='green', s=1)
-    # ax.scatter(x_values, [weighted_nearest_neighbors(training_set, x) for x in x_values], color='red', s=1)
-    # ax.scatter(x_values, [sqr_weighted_nearest_neighbors(training_set, x) for x in x_values], color='purple', s=1)
-    ax.scatter(x_values, [exp_weighted_nearest_neighbors(training_set, x) for x in x_values], color='yellow', s=1)
-    ax.scatter(x_values, [exp_sqr_weighted_nearest_neighbors(training_set, x) for x in x_values], color='pink', s=1)
-    ax.plot(x_values, [ols(training_set, x) for x in x_values], color='black')
-    plt.show()
 
 
